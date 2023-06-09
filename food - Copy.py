c7b9c730679b84d787cd8e74055644bfc0ebabbb
HarshitWaldia <106975419+HarshitWaldia@users.noreply.github.com>
2023-06-09 20:09:23 +0530

    cleaned code

--- food - Copy.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.corpus import wordnet
-
-synonyms = []
-for syn in wordnet.synsets('food'):
-    for lemma in syn.lemmas():
-        synonyms.append(lemma.name())
-print(synonyms)
